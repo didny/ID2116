c5dfc931f0c885f6638f0a626b28ce1502a436d5
Nakayama Yuta <didny@nus.edu.sg>
2020-03-05 14:26:25 +0800

    updated links for team websites

---
+++ scripts/progress.js
@@ -0,0 +1,195 @@
+progress = [
+    {
+        "TeamID": "1",
+        "GitHub": "https://natnell.github.io/id2116/",
+        "ProfilePages": [
+            "https://muginana.github.io/link/",
+            "https://natnell.github.io/Portfolio/"
+        ]
+    },
+    {
+        "TeamID": "3",
+        "GitHub": "https://kohbeining1.github.io/id2116/",
+        "ProfilePages": [
+            "https://kohbeining1.github.io/id2116/kohbeining.html",
+            "https://kohbeining1.github.io/id2116/adereschua.html"
+        ]
+    },
+    {
+        "TeamID": "4",
+        "GitHub": "https://tayyqi.github.io/id2116",
+        "ProfilePages": [
+            "https://tayyqi.github.io/id2116/yingqi.html",
+            "https://tayyqi.github.io/id2116/john.html"
+        ]
+    },
+    {
+        "TeamID": "5",
+        "GitHub": "https://ziningyap.github.io/id2116",
+        "ProfilePages": [
+            "https://ziningyap.github.io/id2116/lewisten.html",
+            "https://ziningyap.github.io/id2116/zining.html"
+        ]
+    },
+    {
+        "TeamID": "6",
+        "GitHub": "https://kiannelim.github.io/id2116/",
+        "ProfilePages": [
+            "https://kiannelim.github.io/id2116/kianne.html",
+            "https://charlotteho.weebly.com/"
+        ]
+    },
+    {
+        "TeamID": "7",
+        "GitHub": "https://siyi10.github.io/id2116/#",
+        "ProfilePages": [
+            "https://siyi10.github.io/id2116/siyi.html",
+            ""
+        ]
+    },
+    {
+        "TeamID": "8",
+        "GitHub": "https://kahmin-jpeg.github.io/ID2116/",
+        "ProfilePages": [
+            "https://kahmin-jpeg.github.io/ID2116/Shaokang.html",
+            "",
+            "https://kahmin-jpeg.github.io/ID2116/kahmin.html"
+        ]
+    },
+    {
+        "TeamID": "9",
+        "GitHub": "https://shanelhan.github.io/id2116/",
+        "ProfilePages": [
+            "https://shanelhan.github.io/id2116/yourname",
+            "https://shanelhan.github.io/id2116/yourname"
+        ]
+    },
+    {
+        "TeamID": "10",
+        "GitHub": "https://brandonows1.github.io/id2116/",
+        "ProfilePages": [
+            "https://brandonows1.github.io/id2116/brandonows.html",
+            ""
+        ]
+    },
+    {
+        "TeamID": "11",
+        "GitHub": "IvanTanGuanQuan.github.io/id2116/",
+        "ProfilePages": [
+            "",
+            "https://IvanTanGuanQuan.github.io/id2116/yongzz.html"
+        ]
+    },
+    {
+        "TeamID": "12",
+        "GitHub": "https://jieraissaying.github.io/id2116/",
+        "ProfilePages": [
+            "https://jieraissaying.github.io/id2116/sarah.html",
+            "https://jieying199.github.io/id2116/"
+        ]
+    },
+    {
+        "TeamID": "13",
+        "GitHub": "https://rachellsimm.github.io/id2116/home.html",
+        "ProfilePages": [
+            "https://rachellsimm.github.io/id2116/jiajienew.html",
+            "https://rachellsimm.github.io/id2116/rachel.html"
+        ]
+    },
+    {
+        "TeamID": "14",
+        "GitHub": "https://kahkiattt.github.io/id2116",
+        "ProfilePages": [
+            "https://kahkiattt.github.io/id2116/kahkiat",
+            "https://shainanananana.github.io/id2116/shaina"
+        ]
+    },
+    {
+        "TeamID": "15",
+        "GitHub": "https://letsmarbyngayandgetiton.github.io/1d2116/",
+        "ProfilePages": [
+            "https://mervynchendx.github.io/id2116/mervyn",
+            "https://letsmarbyngayandgetiton.github.io/1d2116/MabelKhaw"
+        ]
+    },
+    {
+        "TeamID": "16",
+        "GitHub": "https://lintsaiwei.github.io/id2116",
+        "ProfilePages": [
+            "https://lintsaiwei.github.io/id2116/tsaiwei.html",
+            "https://lintsaiwei.github.io/id2116/averylin.html"
+        ]
+    },
+    {
+        "TeamID": "17",
+        "GitHub": "https://skayrien.github.io/id2116/index.html",
+        "ProfilePages": [
+            "https://skayrien.github.io/id2116/tayjohan.html",
+            "https://skayrien.github.io/id2116/samuel.html"
+        ]
+    },
+    {
+        "TeamID": "18",
+        "GitHub": "https://lzyingl.github.io/id2116/",
+        "ProfilePages": [
+            "",
+            "https://lzyingl.github.io/id2116/zhiying"
+        ]
+    },
+    {
+        "TeamID": "19",
+        "GitHub": "https://hana-amran.github.io/id2116/Group%20Folder/Hana-Sahrah.html",
+        "ProfilePages": [
+            "",
+            "https://hana-amran.github.io/id2116/project-2/hana.html"
+        ]
+    },
+    {
+        "TeamID": "20",
+        "GitHub": "https://jiahui-id.github.io/id2116/",
+        "ProfilePages": [
+            "https://jiahui-id.github.io/id2116/shiehchih.html",
+            "https://jiahui-id.github.io/id2116/jiahui.html"
+        ]
+    },
+    {
+        "TeamID": "21",
+        "GitHub": "https://leongdarryl.github.io/id2116/",
+        "ProfilePages": [
+            "https://leongdarryl.github.io/id2116/MoJiSang.html ",
+            "https://gwenkeirapang.github.io/gwen/gwen.html"
+        ]
+    },
+    {
+        "TeamID": "22",
+        "GitHub": "https://nyshaaa.github.io/id2116_Morning-Business/1_final_website_stuff/new_morningbusiness/new_morningbusiness.html",
+        "ProfilePages": [
+            "https://nyshaaa.github.io/id2116_Morning-Business/nysha2.html",
+            "https://nyshaaa.github.io/id2116_Morning-Business/anqi.html"
+        ]
+    },
+    {
+        "TeamID": "23",
+        "GitHub": "https://xiaoxuaan.github.io/id2116/",
+        "ProfilePages": [
+            "https://xiaoxuaan.github.io/id2116/xiaoxuan.html",
+            ""
+        ]
+    },
+    {
+        "TeamID": "24",
+        "GitHub": "https://e0407313.github.io/id2116/",
+        "ProfilePages": [
+            "https://e0407313.github.io/id2116/guozi.html",
+            "https://e0407313.github.io/id2116/Lester.html"
+        ]
+    },
+    {
+        "TeamID": "25",
+        "GitHub": "https://cchloechanc.github.io/id2116",
+        "ProfilePages": [
+            "https://cchloechanc.github.io/id2116/chloe.html",
+            "https://cchloechanc.github.io/id2116/junpeng.html"
+        ]
+    }
+]
